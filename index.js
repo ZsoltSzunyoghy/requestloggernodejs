428ec9a3ddb5a1bb8235b2f1e771ad8df1cbeef9
Zsolt Szunyoghy <zsolt.szunyoghy@sap.com>
2021-03-07 14:27:37 +0100

    added morgan-body to parse the request body

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
 
+const bodyparser1 = require('body-parser');
+const bodyparser = bodyparser1();
+
+const morgan = require('morgan');
+const morganbody = require('morgan-body');
+
+
+app.use(morgan('combined'));
+app.use(bodyparser);
+morganbody(app);
+
+// parse the body using own middleware:
+/*app.use((req, res, next) => {
+    console.log(req.body);
+    next();
+  });*/
+
 app.get('/', function (req, res) {
   res.send('Hello ddd World!');
-  console.log("received GET request on path /");
 });
 
 const port = process.env.PORT || 3000;
